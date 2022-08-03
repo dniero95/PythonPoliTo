469e25303948f8a3d065cccc5c8a7dd6655763cc
dniero95 <darioniero@icloud.com>
2022-08-03 12:49:58 +0200

    update es_05 lab03 done version with one line if

--- lab03/es_05/main.py
+++ lab03/es_05/main.py
@@ -1,12 +1,21 @@
 if __name__ == '__main__':
     year = int(input('Anno = '))
 
-    if year > 1582:
-        if year % 4 == 0:
-            if year % 100 == 0:
-                if year % 400 == 0:
-                    print(f'Il {year} é un anno bisestile!')
-                else:
-                    print(f'Il {year} non é un anno bisestile!')
-            else:
-                print(f'Il {year} é un anno bisestile!')
+    # if year > 1582:
+    #     if year % 4 == 0:
+    #         if year % 100 == 0:
+    #             if year % 400 == 0:
+    #                 print(f'Il {year} é un anno bisestile!')
+    #             else:
+    #                 print(f'Il {year} non é un anno bisestile!')
+    #         else:
+    #             print(f'Il {year} é un anno bisestile!')
+    #
+
+    # Versione con un unico if
+    if year > 1582 and year % 4 == 0 and (not year % 100 == 0 or year % 400 == 0):
+        print(f'Il {year} é un anno bisestile!')
+    else:
+        print(f'Il {year} non é un anno bisestile!')
+
+
