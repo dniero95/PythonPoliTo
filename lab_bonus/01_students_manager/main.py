d0d652ce0201416b5338983d4c71aac4cb405155
dniero95 <darioniero@icloud.com>
2022-08-27 17:02:14 +0200

    add __repr__ method to student

--- lab_bonus/01_students_manager/main.py
+++ lab_bonus/01_students_manager/main.py
@@ -1,9 +1,16 @@
 import csv
 import sys
+from student import Student
 
 from sm_functions import print_menu
 
 if __name__ == '__main__':
+
+    # Create list of students
+    with open('students.csv', 'r') as file:
+        students = file.readlines()
+        for student in students:
+            Student(student[0], student[1], student[2], student[3], student[4])
     print_menu() #Stampa il menù
     # Memorizza l'opzione
     while True:
@@ -12,11 +19,8 @@ if __name__ == '__main__':
             print('Arrivederci')
             sys.exit() # Termina il programma
         elif option == 1:
-            with open('students.csv', 'r') as file:
-                reader = csv.DictReader(file)
-                students = list(reader)
-                for student in students:
-                    print((student))
+            for student in Student.all:
+                print(student)
         elif option == 2:
             pass
         elif option == 3:
--- lab_bonus/01_students_manager/student.py
+++ lab_bonus/01_students_manager/student.py
@@ -8,3 +8,9 @@ class Student:
         self.surname = surname
         self.email = email
         self.school_class = school_class
+
+        Student.all.append(self)
+
+    # Represent student obj
+    def __repr__(self):
+        return f'{self.registration_number}; {self.name}; {self.surname}; {self.email}; {self.school_class}'
