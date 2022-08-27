ee623f9c655f73623dadf9542d33ea9906f5adcb
dniero95 <darioniero@icloud.com>
2022-08-27 17:55:19 +0200

    finish 01_students_manager; need improvment

--- lab_bonus/01_students_manager/main.py
+++ lab_bonus/01_students_manager/main.py
@@ -9,8 +9,13 @@ if __name__ == '__main__':
     # Create list of students
     with open('students.csv', 'r') as file:
         students = file.readlines()
+        students.remove(students[0])
         for student in students:
+            student = student.replace('\n', '')
+            student = student.split((';'))
             Student(student[0], student[1], student[2], student[3], student[4])
+
+        print(f'{Student.students_number} studenti trovati')
     print_menu() #Stampa il menù
     # Memorizza l'opzione
     while True:
@@ -22,10 +27,19 @@ if __name__ == '__main__':
             for student in Student.all:
                 print(student)
         elif option == 2:
-            pass
+            class_code = input('Classe: ')
+            for student in Student.all:
+                if student.school_class == class_code:
+                    print(student)
         elif option == 3:
-            pass
+            first_char_surname = input('Prima Lettera Cognome: ')
+            for student in Student.all:
+                if student.surname[0] == first_char_surname:
+                    print(student)
         elif option == 4:
-            pass
+            Student.all.sort(key=lambda student: student.school_class)
+            for student in Student.all:
+                print(student)
         elif option == 5:
-            pass
+            with open('studenti-quarta.txt', 'w') as file:
+                file.writelines(str([student for student in Student.all if int(student.school_class[0]) == 4]).replace(',', '\n')[1:-2])
--- lab_bonus/01_students_manager/student.py
+++ lab_bonus/01_students_manager/student.py
@@ -1,6 +1,6 @@
 class Student:
     all = []
-
+    students_number = 0
     # I define the constructor
     def __init__(self, registration_number: int, name: str, surname: str, email: str, school_class: str):
         self.registration_number = registration_number
@@ -10,7 +10,7 @@ class Student:
         self.school_class = school_class
 
         Student.all.append(self)
-
+        Student.students_number += 1
     # Represent student obj
     def __repr__(self):
         return f'{self.registration_number}; {self.name}; {self.surname}; {self.email}; {self.school_class}'
