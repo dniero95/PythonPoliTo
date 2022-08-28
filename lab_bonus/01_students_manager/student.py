800e7e0e48282935dc3de684f98253776ad77f4e
dniero95 <darioniero@icloud.com>
2022-08-28 12:45:23 +0200

    add classmethod instatiate_from_csv

--- lab_bonus/01_students_manager/student.py
+++ lab_bonus/01_students_manager/student.py
@@ -1,4 +1,8 @@
+import csv
+
+
 class Student:
+    # Store all students and how many there are here
     all = []
     students_number = 0
     # I define the constructor
@@ -11,6 +15,24 @@ class Student:
 
         Student.all.append(self)
         Student.students_number += 1
+
+
+    # Instantiete students from csv file
+
+    @classmethod
+    def instantiate_from_csv(cls, path: str):
+        with open(path, 'r') as file:
+            reader = csv.DictReader(file)
+            students = list(reader)
+
+            for student in students:
+                Student(
+                    registration_number=int(student.get('registration_number')),
+                    name=student.get('name'),
+                    surname=student.get('surname'),
+                    email=student.get('email'),
+                    school_class=student.get('school_class')
+                )
     # Represent student obj
     def __repr__(self):
         return f'{self.registration_number}; {self.name}; {self.surname}; {self.email}; {self.school_class}'
