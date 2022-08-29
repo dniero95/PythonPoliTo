e79db771f69284574d327bb067ad4eedb90ba6be
dniero95 <darioniero@icloud.com>
2022-08-29 16:20:12 +0200

    add product class

---
+++ lab_bonus/02_business_manager/product.py
@@ -0,0 +1,10 @@
+class Product:
+    # proprietà: Codice, Prodotto, Descrizione, Prezzo metodi: ToString
+    def __init__(self, code:str, name:str, description: str, price:float):
+        self.code = code
+        self.name = name
+        self.description = description
+        self.price = price
+
+    def __repr__(self):
+        return f'Product({self.code}, {self.name}, {self.description}, {self.price})'
