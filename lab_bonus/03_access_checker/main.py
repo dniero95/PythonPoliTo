96dd8838bbbb196f758e52ad62556f091d00e680
dniero95 <darioniero@icloud.com>
2022-08-30 18:56:59 +0200

    start implementing argparse

--- lab_bonus/03_access_checker/main.py
+++ lab_bonus/03_access_checker/main.py
@@ -1,15 +1,24 @@
-
+import argparse as args
 from access import Access
 if __name__ == '__main__':
-    # read data from file access.txt
-    file_name = 'access.txt'
-    Access.instantiate_from_txt(file_name)
+    # handle the argument from CLI
+    parser = args.ArgumentParser(description='list of users name')
+    parser.add_argument('users', metavar='users', type=str, nargs='+', help='Enter users as: name1 name2 name3')
+    args = parser.parse_args()
+    users = args._get_args()
+    print(users)
+    print(type(args))
 
-    users = ['rossi', 'bianchi', 'verdi']
-    # print anomalies
-    print(f'Anomalie rilevate:\n{Access.format_anomalies(users)}')
-    print(f'Numero totale di anomalie: {Access.number_of_anomalies(users)}')
-    print(f'Utente con il maggior numero di anomalie: {Access.max_number_of_anomalies(users)}')
+
+
+    # read data from file access.txt
+    # file_name = 'access.txt'
+    # Access.instantiate_from_txt(file_name)
+    #
+    # # print anomalies
+    # print(f'Anomalie rilevate:\n{Access.format_anomalies(users)}')
+    # print(f'Numero totale di anomalie: {Access.number_of_anomalies(users)}')
+    # print(f'Utente con il maggior numero di anomalie: {Access.max_number_of_anomalies(users)}')
 
 
 #     todo: implement argparse
