bcead17eeaaa721b93615dc9686391cd72d8835a
dniero95 <darioniero@icloud.com>
2022-08-31 15:40:22 +0200

    remove print user list

--- lab_bonus/03_access_checker/main.py
+++ lab_bonus/03_access_checker/main.py
@@ -6,9 +6,7 @@ if __name__ == '__main__':
     parser = args.ArgumentParser(description='script that analize anomalies in the access of a list of user taken from a access.txt file')
     parser.add_argument('users', type=str, nargs='+', help='Enter users as: name1 name2 name3')
     parsed_args = parser.parse_args()
-    print(parsed_args.users)
     users = parsed_args.users
-    # users = ['verdi', 'rossi', 'bianchi', 'gialli']
     # read data from file access.txt
     file_name = 'access.txt'
     Access.instantiate_from_txt(file_name)
